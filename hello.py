4f68c1b6a5922ff432c3f2b1dbd1b4909e0d1252
lilin <2071498859@qq.com>
2020-06-23 20:05:33 +0800

    1

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print("hello world")
+# # print("hello world")
+# #打印
+# '''这是多行注释
+# '''
+# a="abcd "+\
+#     "edf"
+# print(a)
+# a1=[1,2,3,4]
+# print(a1)
+# a=0
+#
+# if a==0:
+#     print(a)
+# else:
+#     print(False)
+# s="abcde"
+# print(s[1:3])
+s="10"
+a=int(s,10)
